701abb2513074331f29ddc535a2d569bc78309f6
Web Designer Rohan <biswasrohan100@gmail.com>
2025-04-20 17:42:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-function switchForm(formType) {
-            const container = document.querySelector('.container');
-            container.classList.remove('sign-in', 'sign-up');
-            container.classList.add(formType);
+function toggleForm() {
+    const container = document.querySelector('.container');
+    container.classList.toggle('active');
+    
+    // Update toggle button text
+    const toggleText = document.getElementById('toggleText');
+    const imgToggleBtn = document.getElementById('imgToggleBtn');
+    
+    if(container.classList.contains('active')) {
+        toggleText.textContent = 'Sign In';
+        imgToggleBtn.textContent = 'Register Now';
+    } else {
+        toggleText.textContent = 'Sign Up';
+        imgToggleBtn.textContent = 'Login Now';
+    }
 }
